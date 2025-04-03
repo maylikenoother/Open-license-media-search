3f364811f86841afd739373a0fdef0568dd184f3
maylikenoother <ogedengbemary19@gmail.com>
2025-04-03 20:41:19 +0100

    updated config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -20,7 +20,7 @@ if (isRender) {
 }
 
 const config = {
-  apiUrl: apiBaseUrl,
+  apiUrl: `${import.meta.env.VITE_API_URL || '/api'}`,
   clerk: {
     publishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
   }
