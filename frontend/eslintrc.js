2ba2379df2dd555811dc8c40f6b8610437a23012
maylikenoother <ogedengbemary19@gmail.com>
2025-04-06 17:35:47 +0100

    Add CI/CD pipeline with Render deploy hooks

---
+++ frontend/eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+      'jest/globals': true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: ['react', 'jest'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
